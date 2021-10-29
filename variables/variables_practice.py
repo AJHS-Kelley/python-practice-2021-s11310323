1144fb7751d9136c8a4cd1105c19c91e5016134a
David mosely <90721327+s11310323@users.noreply.github.com>
2021-10-29 14:39:37 -0400

    V0.1 {4A]
    added three types of data

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables practice, david mosely, 10/29/21 2.22pm, verison 0.0  
+# Variables practice, david mosely, 10/29/21 2.37pm, verison 0.1  
+
+# three common types of data 
+
+lives_remaining  = 10 # INTERGERS, any = +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "curse you , bill! I hate youu!"
+# line 7 is a string -- collection of letters/numbers/other chars.
